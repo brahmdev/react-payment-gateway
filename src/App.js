f0b26cfe330f3f98f30ec6b766e4a86eece9e809
brahmdev <pandey.dev25@gmail.com>
2020-10-28 07:38:33 +0100

    event handling moved to loadDibs.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import loadDibs from './loadDibs';
 import './App.css';
 
@@ -10,30 +9,10 @@ const App = () => {
     if (!loaded) {
       loadDibs(() => {
         setLoaded(true);
-        //window.cleanUp('027400005f97b07bcbcf1f1738af2dfb');
-        /* other code */
-        window.initCheckout({
-          checkoutKey: 'test-checkout-key-d6f65d6024b44becb6a4f5ad5fee8d2a', //[Required] Test or Live GUID with dashes
-          paymentId: '027400005f97b07bcbcf1f1738af2dfb', //[required] GUID without dashes
-          partnerMerchantNumber: '100018348', //[optional] Number
-          containerId: 'dibs-complete-checkout', //[optional] defaultValue: dibs-checkout-content
-          language: 'en-GB', //[optional] defaultValue: en-GB
-        });
       });
     } else {
     }
   }, [loaded]);
-
-  const initPayments = () => {
-    console.log('loaded :: ', loaded);
-    if (loaded) {
-      //this is the event that the merchant should listen to redirect to the “payment-is-ok” page
-      /* checkout.on('payment-completed', function (response) {
-        console.log('RESPONSE ON PAYMENT COMPLETED :: ', response);
-        window.location = '/PaymentSuccessful';
-      }); */
-    }
-  };
 
   return (
     <div className="App">
--- src/loadDibs.js
+++ src/loadDibs.js
@@ -7,6 +7,23 @@ const loadDibs = (callback) => {
     document.body.appendChild(script);
     script.onload = () => {
       console.log('DIBS LOADED');
+      const checkoutOptions = {
+        checkoutKey: '<CHECKOUT_KEY_HERE>', //[Required] Test or Live GUID with dashes
+        paymentId: '<PAYMENT_ID_HERE_', //[required] GUID without dashes
+        containerId: 'dibs-complete-checkout', //[optional] defaultValue: dibs-checkout-content
+        language: 'en-GB', //[optional] defaultValue: en-GB
+      };
+      const dibsObject = window.Dibs;
+      var checkout = new dibsObject.Checkout(checkoutOptions);
+      checkout.on('payment-completed', function (response) {
+        console.log('RESPONSE ON PAYMENT COMPLETED :: ', response);
+      });
+      checkout.on('address-changed', function (response) {
+        console.log('RESPONSE ON address-changed :: ', response);
+      });
+      checkout.on('payment-cancelled', function (response) {
+        console.log('RESPONSE ON payment-cancelled :: ', response);
+      });
       if (callback) callback();
     };
   }
